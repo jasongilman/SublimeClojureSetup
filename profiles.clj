a7a6b92cb8be8ea56fb02bf38b9a96452e965efa
Jason Gilman <jason@element84.com>
2014-01-03 07:11:54 -0500

    Added debug-repl as a dependency.

--- profiles.clj
+++ profiles.clj
@@ -1,4 +1,4 @@
-{:user {:dependencies []
+{:user {:dependencies [[org.clojars.gjahad/debug-repl "0.3.3"]]
         :injections [(use '[clojure.pprint :only [pp pprint]])
                      (use 'alex-and-georges.debug-repl)]
         :plugins [[lein-ancient "0.5.2"]]}}
