d6ca513f3f9b7bad76628da8544ec89b2e2d4ddc
Jason Gilman <jason@element84.com>
2014-04-21 14:50:48 -0400

    Added a key binding to test a single test.

--- ClojureHelpers.py
+++ ClojureHelpers.py
@@ -3,6 +3,14 @@ from sublimerepl import manager
 
 REFRESH_NAMESPACES_CMD = "(let [r 'user/reset] (if (find-var r) ((resolve r)) (clojure.tools.namespace.repl/refresh :after r)))"
 
+def selected_text(s):
+  v = s.view
+  parts = [v.substr(region) for region in v.sel()]
+  return "".join(parts)
+
+def repl_external_id(s):
+    return s.view.scope_name(0).split(" ")[0].split(".", 1)[1]
+
 # Allows running an arbitrary command in the REPL but not in the current namespace
 # Example key binding:
 # { "keys": ["alt+super+r"], "command": "run_command_in_repl", "args": {"command": "(user/reset)"}},
@@ -37,11 +45,21 @@ class RunCommandInNamespaceInReplCommand(text_transfer.ReplSend):
     if refresh_namespaces:
       self.view.window().run_command("refresh_namespaces_in_repl")
 
-    external_id = self.repl_external_id()
+    external_id = repl_external_id(self)
     super( RunCommandInNamespaceInReplCommand, self ).run(edit, external_id, command)
 
-  def repl_external_id(self):
-    return self.view.scope_name(0).split(" ")[0].split(".", 1)[1]
+
+class TestSelectedVarInReplCommand(text_transfer.ReplSend):
+  def run(self, edit, refresh_namespaces=False):
+
+    if refresh_namespaces:
+      self.view.window().run_command("refresh_namespaces_in_repl")
+
+    selected = selected_text(self)
+    text = "(do (test-var #'" + selected +") (println \"tested " + selected +"\"))"
+    external_id = repl_external_id(self)
+    super(TestSelectedVarInReplCommand, self).run(edit, external_id, text)
+
 
 # Allows running a function specified in arguments on the current text selected in the repl.
 # Example key binding:
@@ -49,17 +67,9 @@ class RunCommandInNamespaceInReplCommand(text_transfer.ReplSend):
 # Would run (clojure.repl/doc <selected_text>) in the repl in the current namespace
 class RunOnSelectionInReplCommand(text_transfer.ReplSend):
   def run(self, edit, function):
-    text = "(" + function + " " + self.selected_text() +")"
-    external_id = self.repl_external_id()
+    text = "(" + function + " " + selected_text(self) +")"
+    external_id = repl_external_id(self)
     super( RunOnSelectionInReplCommand, self ).run(edit, external_id, text)
-
-  def selected_text(self):
-    v = self.view
-    parts = [v.substr(region) for region in v.sel()]
-    return "".join(parts)
-
-  def repl_external_id(self):
-    return self.view.scope_name(0).split(" ")[0].split(".", 1)[1]
 
 # Loads the current file in the REPL by telling the REPL to load it using the complete path
 # This is much faster than the built in sublime repl command which copies the entire file into the
@@ -74,16 +84,9 @@ class LoadFileInReplCommand(text_transfer.ReplTransferCurrent):
 class LoadSelectionInReplCommand(text_transfer.ReplSend):
   def run(self, edit):
     f = open("/tmp/selection.clj", "w")
-    f.write(self.selected_text())
+    f.write(selected_text(self))
     f.close()
     form = "(load-file \"" + f.name +"\")"
-    external_id = self.repl_external_id()
+    external_id = repl_external_id(self)
     super( LoadSelectionInReplCommand, self ).run(edit, external_id, form)
 
-  def selected_text(self):
-    v = self.view
-    parts = [v.substr(region) for region in v.sel()]
-    return "".join(parts)
-
-  def repl_external_id(self):
-    return self.view.scope_name(0).split(" ")[0].split(".", 1)[1]
